578bd6c512382f1e6165d4c9649d5b19233d27b5
DenisSagitov <d.sagitov@aventica.ru>
2023-03-04 13:36:40 +0300

    add protocol to local file in puppeteer

--- services/browser.js
+++ services/browser.js
@@ -9,47 +9,54 @@ class Browser {
   }
 
   async getScreenshot(options) {
-    const page = await this.browser.newPage()
-    await page.setViewport({ width: 724, height: 441 })
-    await page.goto(
-      path.resolve(process.cwd(), 'static', `template.html`),
-      { waitUntil: 'networkidle0' }
-    )
+    let path = 'file://' + path.resolve(process.cwd(), 'static', `template.html`)
+    try {
+      const page = await this.browser.newPage()
+      await page.setViewport({ width: 724, height: 441 })
+      await page.goto(
+        path,
+        { waitUntil: 'networkidle0' }
+      )
 
-    await page.evaluate(async options => {
-      const
-        img1 = document.querySelector('#INJECT_QUESTION_AVATAR'),
-        img2 = document.querySelector('#INJECT_ANSWER_AVATAR')
+      await page.evaluate(async options => {
+        const
+          img1 = document.querySelector('#INJECT_QUESTION_AVATAR'),
+          img2 = document.querySelector('#INJECT_ANSWER_AVATAR')
 
-      img1.src = options.avatarFirst
-      img2.src = options.avatar
+        img1.src = options.avatarFirst
+        img2.src = options.avatar
 
-      await Promise.all([img1, img2].map(img => {
-        if (img.complete)
-          return
-        return new Promise((ok, bad) => {
-          img.addEventListener('load', ok)
-          img.addEventListener('error', bad)
+        await Promise.all([img1, img2].map(img => {
+          if (img.complete)
+            return
+          return new Promise((ok, bad) => {
+            img.addEventListener('load', ok)
+            img.addEventListener('error', bad)
+          })
+        }))
+
+        ;[
+          ['#INJECT_QUESTION_USERNAME', options.usernameFirst],
+          ['#INJECT_ANSWER_USERNAME', options.username],
+          ['#INJECT_QUESTION_TEXT', options.contentFirst],
+          ['#INJECT_ANSWER_TEXT', options.content]
+        ].forEach(([ selector, text ]) => {
+          document.querySelector(selector).innerText = text
         })
-      }))
+      }, options)
 
-      ;[
-        ['#INJECT_QUESTION_USERNAME', options.usernameFirst],
-        ['#INJECT_ANSWER_USERNAME', options.username],
-        ['#INJECT_QUESTION_TEXT', options.contentFirst],
-        ['#INJECT_ANSWER_TEXT', options.content]
-      ].forEach(([ selector, text ]) => {
-        document.querySelector(selector).innerText = text
+      const imageBuffer = await page.screenshot({
+        type: 'jpeg',
+        quality: 50,
+
       })
-    }, options)
+      await page.close()
+      return imageBuffer
+    }
+    catch (e) {
+      throw new Error(`${e.message}. Path: ${path}`)
+    }
 
-    const imageBuffer = await page.screenshot({
-      type: 'jpeg',
-      quality: 50,
-
-    })
-    await page.close()
-    return imageBuffer
   }
 
 }
